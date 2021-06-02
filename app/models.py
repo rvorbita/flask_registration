292753f97423306727fe0bcb3422110e8d361d92
Raymart O <71857825+rvorbita@users.noreply.github.com>
2021-06-02 23:11:41 +0800

    Add Database model

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+
+class User(db.Model):
+    """User model"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), index=True, unique=True)
+    email = db.Column(db.String(180), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+
+    def __repr__(self):
+
+        return f"User: {self.username}"
+
+
+        
